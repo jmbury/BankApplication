4ead257d2e46c841f580ea3bdbc9069849833067
jmbury <justyna.m.bury@gmail.com>
2022-03-10 20:20:39 +0100

    get_set_books

--- src/Books.java
+++ src/Books.java
@@ -1,3 +1,31 @@
 public class Books {
+    String bookName;
+    String author;
+    int numberOfPages;
+
+    public Books(String bookName, String author, int numberOfPages) {
+        this.bookName = bookName;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+
+
+    }
+    public String toString(){
+        return "tytuł: " +bookName + "Autor: " + author + "Liczba stron: " + numberOfPages;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+
+    public int getNumberOfPages(){
+        return numberOfPages;
+    }
+    public void setNumberOfPages(int numberOfPages){
+        this.numberOfPages=numberOfPages;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+        Books book1 = new Books ("1984 ", "Orwell ", 250);
+        Books book2 = new Books("Alicja w krainie czarów ", "Carroll ", 136);
+        Books book3 = new Books("Nowy Wspaniały Świat ", "Huxley ", 176);
+
+        System.out.println(book1.toString());
+        book1.setAuthor("George Orwell ");
+        System.out.println(book1.toString());
+
+        System.out.println(book2.toString());
+        System.out.println(book2.getNumberOfPages());
+        book2.setNumberOfPages(567);
+        System.out.println(book2.toString());
+    }
 
 }
